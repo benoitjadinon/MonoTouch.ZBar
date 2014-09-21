e6e9492b3a24b945a4cd1d90294b5a2cddcba647
Benoit Jadinon <j@dinonbeno.it>
2014-09-21 20:29:11 +0200

    fix to compile and run on simulator and phone

---
+++ binding/ZBarSymbol.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MonoTouch.ObjCRuntime;
+
+namespace ZBar
+{
+	public partial class ZBarSymbol {
+		public static ZBarSymbol Create (IntPtr symbol) { 
+			ZBarSymbol a = new ZBarSymbol(); 
+			var ptr = Messaging.IntPtr_objc_msgSend (Class.GetHandle (a.GetType ()), Selector.GetHandle ("alloc")); 
+			ptr = Messaging.IntPtr_objc_msgSend_IntPtr (ptr, Selector.GetHandle ("initWithSymbol:"), symbol); 
+			var obj = new ZBarSymbol (ptr); 
+			obj.DangerousRelease (); // this is because we get a ref when creating/initing the object manually above 
+			return obj; 
+		}
+	}
+
+}
